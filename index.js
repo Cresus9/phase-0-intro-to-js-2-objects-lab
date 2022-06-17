c0607b1277feb93a6b9e132517381d5648ec590f
Sigui Thierry Herman Yabre <thierry.yabre@yahoo.fr>
2022-06-16 21:24:37 -0400

    commit tree 20011c099daa33db53d0892fa49bee376c1033c6

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your solution in this file!
+const employee = {
+    name: "Paul",
+    streetAddress: "943 Main st",
+};
+function updateEmployeeWithKeyAndValue(object, key, value){
+    return {...employee,
+        ["name"]:"Sam",
+        ["streetAddress"]:"11 Broadway",
+    }
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value){
+    obj[key]=value;
+    return obj
+
+
+    }
+function deleteFromEmployeeByKey(obj, key ){
+    obj.key = undefined
+    return obj
+    }
+
+function destructivelyDeleteFromEmployeeByKey(obj, key) {
+    obj[`${key}`]= undefined
+    return obj
+} 
